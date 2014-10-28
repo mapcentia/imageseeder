1e4dc216abf2fdab4a7804c154bb57ba3e40a588
Martin Høgh <mh@mapcentia.com>
2014-10-28 12:14:09 +0100

    no message

--- app/api/v1/Seeder.php
+++ app/api/v1/Seeder.php
@@ -29,16 +29,28 @@ class Seeder extends \app\inc\Controller
     public function get_url()
     {
         $layers = (array)json_decode(urldecode(Input::get("layers")));
-        $bbox = urldecode(Input::get("bbox"));
+
+        $bbox = explode(",", urldecode(Input::get("bbox")));
         $db = urldecode(Input::get("db"));
-        $baseLayer = urldecode(Input::get("baseLayer"));
+        $baseLayer = urldecode(Input::get("baselayer"));
         $size = urldecode(Input::get("size"));
         $temp = array();
+        $dbObj = new \app\inc\Model();
+        $sql = "SELECT
+            ST_X(ST_transform(ST_geomfromtext('POINT({$bbox[0]} {$bbox[1]})',4326),900913)) as x1,
+            ST_Y(ST_transform(ST_geomfromtext('POINT({$bbox[0]} {$bbox[1]})',4326),900913)) as y1,
+            ST_X(ST_transform(ST_geomfromtext('POINT({$bbox[2]} {$bbox[3]})',4326),900913)) as x2,
+            ST_Y(ST_transform(ST_geomfromtext('POINT({$bbox[2]} {$bbox[3]})',4326),900913)) as y2
+            ";
+        $row = $dbObj->fetchRow($dbObj->execQuery($sql));
+        //print_r($row);
+        $bboxStr = implode(",",$row);
+
         for ($i = 0; $i < sizeof($layers); $i++) {
             $temp[] = $layers[$i]->name;
         }
         $layersStr = implode(",", $temp);
-        $url = "{$this->host}/api/v1/staticmap/png/{$db}?baselayer={$baseLayer}&layers={$layersStr}&size={$size}&bbox={$bbox}&lifetime=9999999";
+        $url = "{$this->host}/api/v1/staticmap/png/{$db}?baselayer={$baseLayer}&layers={$layersStr}&size={$size}&bbox={$bboxStr}&lifetime=9999999";
 
         echo Response::passthru("<img class=\"imageseeder\"src=\"{$url}\"/>");
         exit();
